4c17df5e4d4cd2b3258daa02b2c7d99e33490756
Radu Temle <radujtemle@gmail.com>
2024-03-21 18:17:26 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,72 @@
+//Fade in effect
+document.addEventListener("DOMContentLoaded", function() {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('in-view');
+            } else {
+                entry.target.classList.remove('in-view');
+            }
+        });
+    }, {
+        threshold: 0.4 // Adjust if needed
+    });
+
+    const sections = document.querySelectorAll('.fade-in');
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
+// Countdown Timer
+var countDownDate = new Date("2024-11-9").getTime(); // Change this to your wedding date
+
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "It's our wedding day!";
+    }
+}, 1000);
+
+// Smooth Scrolling
+$('a[href*="#"]').click(function(event) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000, function() {
+                var $target = $(target);
+                $target.focus();
+                if ($target.is(":focus")) {
+                    return false;
+                } else {
+                    $target.attr('tabindex','-1');
+                    $target.focus();
+                }
+            });
+        }
+    }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navToggle = document.querySelector('.nav-toggle');
+    const navUl = document.querySelector('nav ul');
+  
+    // Toggle the 'active' class on the nav-toggle button and the visibility of the nav ul
+    navToggle.addEventListener('click', () => {
+      navToggle.classList.toggle('active');
+      navUl.classList.toggle('active');
+    });
+  });
+  
