0b299a8e6e59139e7166c51f9ec9437e3cfbdd25
Radu Temle <radujtemle@gmail.com>
2024-08-03 18:11:36 +0300

    fixed a bug where the gallery button didnt work

--- script.js
+++ script.js
@@ -44,56 +44,6 @@ btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
 
-//Smooth scrolling
-//Needed for old browsers
-const allLinks = document.querySelectorAll("a:link");
-
-allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    const href = link.getAttribute("href");
-
-    // Scroll back to top
-    if (href === "#")
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-
-    // Scroll to other links
-    if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
-    }
-
-    // Close mobile naviagtion
-    if (link.classList.contains("main-nav-link"))
-      headerEl.classList.toggle("nav-open");
-  });
-});
-
-//Sticky navigation
-
-// const sectionHeroEl = document.querySelector(".section-hero");
-
-// const observer = new IntersectionObserver(
-//   function (entries) {
-//     const ent = entries[0];
-//     if (!ent.isIntersecting) {
-//       document.body.classList.add("sticky");
-//     } else {
-//       document.body.classList.remove("sticky");
-//     }
-//   },
-//   {
-//     root: null,
-//     threshold: 0,
-//     rootMargin: "-80px",
-//   }
-// );
-
-// observer.observe(sectionHeroEl);
-
 function initMap() {
   const location = { lat: 47.4377691, lng: 23.2969981 };
 
@@ -130,7 +80,7 @@ function initMap() {
 
 
 document.addEventListener('click', ()=> {
-  
+
 })
 
 // document.addEventListener("DOMContentLoaded", function () {
